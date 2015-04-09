9b505659e57d34404c881400f8cfe0b939f51388
lemonyuhao <15675094429@163.com>
2015-04-09 20:59:08 +0800

    hello

--- src/com/yuhao/news/MainActivity.java
+++ src/com/yuhao/news/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        //你好  这里只是一个演示文件
     }
 
 	private void initView() {
